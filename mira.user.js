3fd000b48b7a953216ddcfbe1c53346b2e920a7e
Solana Boy <29808839+solana-boy@users.noreply.github.com>
2026-03-10 00:30:34 +0300

    Create mira.user.js

---
+++ mira.user.js
@@ -0,0 +1,100 @@
+// ==UserScript==
+// @name         mira
+// @version      0.1
+// @author       Solana-boy
+// @match        https://app.mira.top/*
+// @downloadURL  https://github.com/solana-boy/test-violentmonkey/raw/main/mira.user.js
+// @updateURL    https://github.com/solana-boy/test-violentmonkey/raw/main/mira.user.js
+// @homepage     https://github.com/solana-boy/test-violentmonkey
+// @grant        none
+
+// ==/UserScript==
+
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+function getRandomDelay(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Триггеры событий
+function triggerEvents(element) {
+  const events = [
+      new PointerEvent('pointerdown', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0.5, pointerType: "touch" }),
+      new MouseEvent('mousedown', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('pointerup', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseup', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new PointerEvent('click', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerout', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new PointerEvent('pointerleave', { bubbles: true, cancelable: true, isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, pointerType: "touch" }),
+      new MouseEvent('mouseout', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 }),
+      new MouseEvent('mouseleave', { bubbles: true, cancelable: true, isTrusted: true, screenX: 182, screenY: 877 })
+  ];
+
+  events.forEach((event, index) => {
+      setTimeout(() => element.dispatchEvent(event), index * 100);
+  });
+}
+
+function sleep(ms = 0) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function connectWallet(){
+  var up2 = Array.from(document.querySelectorAll("div")).filter(el => el.textContent == "Wallet On" || el.textContent == "Wallet in")
+  if (up2.length != 0){
+    triggerEvents(up2[0]);
+    await sleep(getRandomDelay(3000, 4100));
+    up2 = Array.from(document.querySelectorAll("button")).filter(el => el.textContent.includes("Open Wallet in Telegram") || el.textContent.includes("Connect Wallet in Telegram on desktop"))
+    if (up2.length != 0){
+      triggerEvents(up2[0]);
+      await sleep(getRandomDelay(10000, 21000));
+    }
+  }
+  await sleep(getRandomDelay(2000, 3100));
+}
+
+async function autoBuy() {
+
+  var up = Array.from(document.querySelectorAll('a[href="/balance"]'));
+  if (up.length != 0){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(4000, 5000));
+  }
+
+  var up = Array.from(document.querySelectorAll("button p")).filter(el => el.textContent == "Claim");
+  if (up.length >= 2){
+    triggerEvents(up[0]);
+    await sleep(getRandomDelay(2000, 3000));
+  }
+
+}
+
+
+function initializeScript() {
+
+    console.log('START claim  ')
+
+    setTimeout(autoBuy, getRandomDelay(23000, 23050));
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeScript);
+} else {
+    initializeScript();
+}
